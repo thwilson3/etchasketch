4d9a3b893df5be4a8a5da3a173e35fcd43fd3b91
thwilson3 <tommyhwilson3@gmail.com>
2022-07-11 15:02:07 -0500

    Make interactive grid

---
+++ script.js
@@ -0,0 +1,17 @@
+let sizeSelection = 16;
+let container = document.querySelector('.grid-container')
+let artBoard = document.querySelectorAll('div.grid')
+
+function grid() {
+    let layout = (sizeSelection * sizeSelection);
+    for (i=1; i<layout; i++) {
+        let square=document.createElement('div')
+        square.classList.add('grid');
+        square.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = "black";
+        })
+        container.appendChild(square)
+    }
+}
+
+grid();
